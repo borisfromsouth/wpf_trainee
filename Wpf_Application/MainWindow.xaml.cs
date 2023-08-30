4ba25b6dd9cd8ad2146c60e19496dca3a949f776
lookaslookas <sashaborisenko@tut.by>
2023-08-30 23:38:29 +0300

    feature: add buttons
    Try add buttons by different ways, add test click method

--- Wpf_Application/MainWindow.xaml.cs
+++ Wpf_Application/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Wpf_Application
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
