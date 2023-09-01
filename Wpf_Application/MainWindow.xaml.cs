ac44f5333187f2c20ad812bebd9b2782ca760056
boris_from_south <sashaborisenko@tut.by>
2023-09-01 16:39:56 +0300

    feature: control GUI from code
    Control GUI from code

--- Wpf_Application/MainWindow.xaml.cs
+++ Wpf_Application/MainWindow.xaml.cs
@@ -23,6 +23,38 @@ namespace Wpf_Application
         public MainWindow()
         {
             InitializeComponent();
+
+            #region Test
+
+            //Grid grid = new Grid();  
+            //this.Content = grid;  // содержимое окна 
+
+            //Button button = new Button();
+            //button.FontSize = 26;
+
+            //WrapPanel wrapPanel = new WrapPanel();
+
+            //TextBlock textBlock = new TextBlock();
+            //textBlock.Text = "Multi";
+            //textBlock.Foreground = Brushes.Blue;
+            //wrapPanel.Children.Add(textBlock);
+
+            //textBlock = new TextBlock();
+            //textBlock.Text = "Color";
+            //textBlock.Foreground = Brushes.Red;
+            //wrapPanel.Children.Add(textBlock);
+
+            //textBlock = new TextBlock();
+            //textBlock.Text = "Button";
+            //textBlock.Foreground = Brushes.White;
+            //wrapPanel.Children.Add(textBlock);
+
+            //button.Content = wrapPanel;
+            //grid.Children.Add(button);
+
+            #endregion
+
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
